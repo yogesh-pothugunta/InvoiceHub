6ff4f9561b96520f0c3383324cf547d72e8dcc88
yogesh-pothugunta <yogeshnaiduyogi07@gmail.com>
2026-06-17 14:53:25 +0530

    Update API URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({ 
-  baseURL: process.env.REACT_APP_API_URL || 'https://invoicehub-backend-7smj.onrender.com/api'
+  baseURL: process.env.REACT_APP_API_URL || 'https://invoicehub-6hhp.onrender.com/api'
 });
 
 API.interceptors.request.use((config) => {
